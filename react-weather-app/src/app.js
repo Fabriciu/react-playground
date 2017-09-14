b687cbdc56726ffb3f2f66390b3012038150d69e
Fabricio Borgatto <fabricioborgatto@gmail.com>
2017-09-14 00:33:49 -0300

    Initial commit

---
+++ react-weather-app/src/app.js
@@ -0,0 +1,14 @@
+import './styles.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+const root = document.getElementById('app-container');
+
+ReactDOM.render(<App />, root);
+
+//ReactDOM.render(<p>Hello world!</p>, root);
+
+
+//console.log('Hello, world!');
